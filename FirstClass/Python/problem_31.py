52df310720acce3305ce93ad4f4454fb10927970
Joan Boulangger <joan.boulangger@gmail.com>
2020-06-17 23:38:57 -0500

    [31 - 32]: Python

---
+++ FirstClass/Python/problem_31.py
@@ -0,0 +1,8 @@
+import numpy as np
+import random
+if __name__ == '__main__':
+    x = np.random.random_integers (100, size = (1, 20))
+    newX = np.sort (x)
+    print (f'Array random: \n{x}')
+    print (f'Array random ordenado: \n{newX}')
+    print (f'El menor numero del array es: {newX[0][0]}')
---
+++ FirstClass/Python/problem_32.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    precmaq = 1
+    while 1 + precmaq > 1:
+        precmaq = precmaq / 2
+    
+    precmaq = 2 * precmaq
+    print (precmaq)
